add19afa2c4d788cd3b4458b5267bd3b1cbdee31
shehraj123 <shehrajsingh@gmail.com>
2022-10-22 14:34:42 -0600

    Add default login and basic interface for user

---
+++ app/src/main/java/com/example/happymeals/User.java
@@ -0,0 +1,50 @@
+package com.example.happymeals;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class User {
+    private String u_id;
+    private String username;
+    private FirebaseFirestore conn;
+
+    private String get_u_id(String username) {
+        // Search up database to get the u_id for the user with username = 'username'
+        return "";
+    }
+
+    public User() {
+        u_id = "Fw8E3ba2rjfSmuOPxxHE"; // Default generated right now (temporary)
+        username = "Guest"; // This is temporary
+        conn = FirebaseFirestore.getInstance();
+    }
+
+    public User(String username) {
+        // login existing user
+        this.username = username;
+        conn = FirebaseFirestore.getInstance();
+        u_id = this.get_u_id(this.username);
+    }
+
+    public String getU_id() {
+        return this.u_id;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public FirebaseFirestore getConn() {
+        return this.conn;
+    }
+
+//    public static User createUser(String username) {
+//        // Create a new user in the database
+//        // Create a User object for that user
+//        // Return that user
+//
+//
+//    }
+
+
+
+}
