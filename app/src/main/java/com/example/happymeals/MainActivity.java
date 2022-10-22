ca49c0329c351f707094af46e2040ca59c14b392
Mohammad <mohammadhammad50@gmail.com>
2022-10-22 16:08:52 -0600

    Add newUser method

--- app/src/main/java/com/example/happymeals/MainActivity.java
+++ app/src/main/java/com/example/happymeals/MainActivity.java
@@ -5,18 +5,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity {
+    Button addUButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addUButton = findViewById(R.id.add_user_button);
+        addUButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                           User user = new User();
+                           user.newUser("Mo");
+                }
+            });
     }
 }
--- app/src/main/java/com/example/happymeals/User.java
+++ app/src/main/java/com/example/happymeals/User.java
@@ -1,11 +1,15 @@
 package com.example.happymeals;
 
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
 
 public class User {
     private String u_id;
     private String username;
     private FirebaseFirestore conn;
+    CollectionReference userRef;
 
     private String get_u_id(String username) {
         // Search up database to get the u_id for the user with username = 'username'
@@ -16,6 +20,7 @@ public class User {
         u_id = "Fw8E3ba2rjfSmuOPxxHE"; // Default generated right now (temporary)
         username = "Guest"; // This is temporary
         conn = FirebaseFirestore.getInstance();
+        userRef = conn.collection("users");
     }
 
     public User(String username) {
@@ -24,6 +29,14 @@ public class User {
         conn = FirebaseFirestore.getInstance();
         u_id = this.get_u_id(this.username);
     }
+
+    public void newUser(String username) {
+        HashMap<String, String> data = new HashMap<>();
+        data.put("username", username);
+        userRef.add(data);
+    }
+
+
 
     public String getU_id() {
         return this.u_id;
