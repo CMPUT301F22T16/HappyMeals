e9dfa4e3e920ede9d72a8d23b23841681b71fc74
shehraj123 <shehrajsingh@gmail.com>
2022-10-22 11:22:24 -0600

    Add Firestore and Analytics support

--- app/src/main/java/com/example/happymeals/MainActivity.java
+++ app/src/main/java/com/example/happymeals/MainActivity.java
@@ -1,10 +1,20 @@
 package com.example.happymeals;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
