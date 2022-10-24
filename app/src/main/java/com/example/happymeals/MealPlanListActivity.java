4d62a9051b414c4575742b22b362577a14e825d7
Jayparkrighthere <zoeytwelve@gmail.com>
2022-10-23 22:01:39 -0600

    meal plan activity and meal plan list activiy init

---
+++ app/src/main/java/com/example/happymeals/MealPlanListActivity.java
@@ -0,0 +1,14 @@
+package com.example.happymeals;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MealPlanListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meal_plan_list);
+    }
+}
