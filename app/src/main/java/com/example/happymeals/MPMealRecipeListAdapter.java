994c157229ae457c6d200d6a8542eebf15975c28
Jayparkrighthere <zoeytwelve@gmail.com>
2022-10-29 22:58:23 -0600

    meal recipe list

---
+++ app/src/main/java/com/example/happymeals/MPMealRecipeListAdapter.java
@@ -0,0 +1,47 @@
+package com.example.happymeals;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.happymeals.databinding.MealRecipeListContentBinding;
+
+import java.util.ArrayList;
+
+import io.grpc.Context;
+
+public class MPMealRecipeListAdapter extends RecyclerView.Adapter<MPMealRecipeListAdapter.MRLViewHolder>{
+    private ArrayList<Recipe> recipes;
+
+    public MPMealRecipeListAdapter(Context context,ArrayList<Recipe> recipes) {this.recipes = recipes;}
+    @NonNull
+    @Override
+    public MPMealRecipeListAdapter.MRLViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new MPMealRecipeListAdapter.MRLViewHolder(MealRecipeListContentBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MPMealRecipeListAdapter.MRLViewHolder holder, int position) {
+        Recipe recipe = recipes.get(position);
+        holder.binding.mpMealRecipeListContentTitle.setText(recipe.getname());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class MRLViewHolder extends RecyclerView.ViewHolder {
+
+        MealRecipeListContentBinding binding;
+
+        public MRLViewHolder(MealRecipeListContentBinding b) {
+            super(b.getRoot());
+            binding = b;
+        }
+    }
+}
--- app/src/main/java/com/example/happymeals/MealRecipeList.java
+++ app/src/main/java/com/example/happymeals/MealRecipeList.java
@@ -1,14 +1,24 @@
 package com.example.happymeals;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.happymeals.databinding.ActivityMealRecipeListBinding;
+
+import java.util.ArrayList;
+
 public class MealRecipeList extends AppCompatActivity {
+    ActivityMealRecipeListBinding activityMealRecipeListBinding;
+    RecyclerView.Adapter mpMealRecipeListAdapter;
+    ArrayList<Recipe> recipes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meal_recipe_list);
+
+        mpMealRecipeListAdapter = new MPMealRecipeListAdapter(this, recipes);
     }
 }
