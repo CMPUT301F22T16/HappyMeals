460c839d0f7cfc19f77a797dbeee9355de6183ca
CarlHat <tianyuan@ualberta.ca>
2022-10-31 18:48:29 -0600

    onclick recycleradapter

--- app/src/main/java/com/example/happymeals/MPMealRecipeListAdapter.java
+++ app/src/main/java/com/example/happymeals/MPMealRecipeListAdapter.java
@@ -2,11 +2,13 @@ package com.example.happymeals;
 
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.happymeals.databinding.ActivityMpmealRecipeListBinding;
 import com.example.happymeals.databinding.MealRecipeListContentBinding;
 
 import java.util.ArrayList;
@@ -14,19 +16,29 @@ import java.util.ArrayList;
 
 public class MPMealRecipeListAdapter extends RecyclerView.Adapter<MPMealRecipeListAdapter.MRLViewHolder>{
     private ArrayList<Recipe> recipes;
+    private ActivityMpmealRecipeListBinding activityMpmealRecipeListBinding;
+    private final View.OnClickListener mOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            int itemPosition = activityMpmealRecipeListBinding.mpRecipeListRecyclerview.getChildLayoutPosition(view);
+        }
+    };
 
+    // indices needs to be added into the arguments.
     public MPMealRecipeListAdapter(Context context, ArrayList<Recipe> recipes) {this.recipes = recipes;}
     @NonNull
     @Override
     public MPMealRecipeListAdapter.MRLViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return new MPMealRecipeListAdapter.MRLViewHolder(MealRecipeListContentBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
+        MealRecipeListContentBinding binding = MealRecipeListContentBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
+        binding.getRoot().setOnClickListener(mOnClickListener);
+        return new MPMealRecipeListAdapter.MRLViewHolder(binding);
 
     }
 
     @Override
     public void onBindViewHolder(@NonNull MPMealRecipeListAdapter.MRLViewHolder holder, int position) {
         Recipe recipe = recipes.get(position);
-        holder.binding.mpMealRecipeListContentTitle.setText(recipe.getname());
+        holder.binding.mpMealRecipeListContentTitle.setText(recipe.getTitle());
 
     }
 
