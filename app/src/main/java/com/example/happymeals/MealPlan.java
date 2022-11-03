2d6ffc3ae472535ad76397159f4a101b47e5a88d
Jayparkrighthere <zoeytwelve@gmail.com>
2022-11-03 00:01:12 -0600

    editing mealplan class

--- app/src/main/java/com/example/happymeals/MealPlan.java
+++ app/src/main/java/com/example/happymeals/MealPlan.java
@@ -3,32 +3,22 @@ package com.example.happymeals;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MealPlan implements Storable{
-    private final List<Meal> breakfast;
-    private final List<Meal> lunch;
-    private final List<Meal> dinner;
+    private final Map<String, List<Meal>> meal_plans;
     private final int num_days;
     private String ump_id = null; // ids' null by default if not fetched
 
-    public MealPlan(List<Meal> breakfast, List<Meal> lunch, List<Meal> dinner, int num_days) {
-        this.breakfast = breakfast;
-        this.lunch = lunch;
-        this.dinner = dinner;
+    public MealPlan(Map<String, List<Meal>> meal_plans, int num_days) {
+        this.meal_plans = meal_plans;
         this.num_days = num_days;
     }
 
-    public List<Meal> getBreakfast() {
-        return breakfast;
+    public Map<String, List<Meal>> getMealPlans(){
+        return this.meal_plans;
     }
 
-    public List<Meal> getLunch() {
-        return lunch;
-    }
-
-    public List<Meal> getDinner() {
-        return dinner;
-    }
 
     public int getNum_days() {
         return num_days;
@@ -64,5 +54,9 @@ public class MealPlan implements Storable{
         data.put("num_days", num_days);
 
         return data;
+    }
+
+    public Map<String, List<Meal>> getMeal_plans() {
+        return meal_plans;
     }
 }
