ae25698412dbba47ecbf55b17e0aee6293c67803
Jayparkrighthere <zoeytwelve@gmail.com>
2022-11-04 07:30:57 -0600

    some tests template

--- app/src/androidTest/java/com/example/happymeals/PickRecipeTests.java
+++ app/src/androidTest/java/com/example/happymeals/PickRecipeTests.java
@@ -4,9 +4,12 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
@@ -19,10 +22,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
-public class MyMealsTest {
+public class PickRecipeTests {
     private Solo solo;
-    public ActivityTestRule<MPMyMealsActivity> rule =
-            new ActivityTestRule<>(MPMyMealsActivity.class, true, true);
+    private Intent intent;
+    public ActivityTestRule<MainActivity> rule =
+            new ActivityTestRule<>(MainActivity.class, true, true);
 
     /**
      * Runs before all tests and creates solo instance.
@@ -30,6 +34,10 @@ public class MyMealsTest {
      */
     @Before
     public void setUp() throws Exception{
+//        intent = new Intent();
+//        Bundle bundle = new Bundle();
+//        bundle.putSerializable("USER", "Guest");
+//        intent.putExtras(bundle);
         solo = new Solo(InstrumentationRegistry.getInstrumentation(),rule.getActivity());
     }
 
@@ -40,6 +48,11 @@ public class MyMealsTest {
     @Test
     public void start() throws Exception{
         Activity activity = rule.getActivity();
+//        intent = new Intent(activity.getApplicationContext(),MPPickRecipeActivity.class);
+//        Bundle bundle = new Bundle();
+//        bundle.putSerializable("USER", "Guest");
+//        intent.putExtras(bundle);
+//        activity.startActivity(intent);
     }
 
     /**
@@ -49,11 +62,10 @@ public class MyMealsTest {
      */
     @Test
     public void testCreateNewMeal(){
-        Activity activity = rule.getActivity();
-
-//        solo.assertCurrentActivity("Wrong Activity",MPMyMealsActivity.class);
-        Button addButton = (Button) solo.getView(R.id.my_meals_add_button);
-//
+        solo.assertCurrentActivity("Wrong Activity",MainActivity.class);
+//        assertTrue(solo.waitForView(RecyclerView.class));
+//        assertTrue(solo.waitForView(Button.class));
+//        Button addButton = (Button) solo.getView(R.id.my_meals_add_button);
 //        solo.clickOnView(addButton);
 //        solo.assertCurrentActivity("Wrong Activity", MPMealRecipeList.class);
     }
