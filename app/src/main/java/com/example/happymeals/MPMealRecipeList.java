ec85403f813f85804b51d61f5e8670670cc57d7f
Jayparkrighthere <zoeytwelve@gmail.com>
2022-11-10 17:30:44 -0700

    create bottom sheet

--- app/src/main/java/com/example/happymeals/MPMealRecipeList.java
+++ app/src/main/java/com/example/happymeals/MPMealRecipeList.java
@@ -6,7 +6,10 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
@@ -17,6 +20,8 @@ import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.happymeals.databinding.ActivityMpmealRecipeListBinding;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -129,12 +134,7 @@ public class MPMealRecipeList extends AppCompatActivity {
 
     private void setOnAddButtonListener() {
         addRecipButton.setOnClickListener(v -> {
-            intent = new Intent(this, MPPickRecipeActivity.class);
-            Bundle bundle = new Bundle();
-            bundle.putSerializable("MEAL", meal);
-            intent.putExtras(bundle);
-            activityLauncher.launch(intent);
-
+            showBottomSheetDialog();
         });
     }
 
@@ -181,6 +181,42 @@ public class MPMealRecipeList extends AppCompatActivity {
                 .setNegativeButton(android.R.string.no, null)
                 .setIcon(android.R.drawable.ic_dialog_alert)
                 .show();
+    }
+    private void showBottomSheetDialog() {
+
+        final BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(this);
+        bottomSheetDialog.setContentView(R.layout.meal_recipe_list_bottom_sheet);
+
+        TextView add_from_recipe = bottomSheetDialog.findViewById(R.id.add_from_recipe_textview);
+        TextView add_from_ingredient = bottomSheetDialog.findViewById(R.id.add_from_ingredient_textview);
+        TextView cancel = bottomSheetDialog.findViewById(R.id.bottom_sheet_cancel);
+        add_from_recipe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bottomSheetDialog.dismiss();
+                intent = new Intent(context, MPPickRecipeActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putSerializable("MEAL", meal);
+                intent.putExtras(bundle);
+                activityLauncher.launch(intent);
+            }
+        });
+
+        add_from_ingredient.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Share is Clicked", Toast.LENGTH_LONG).show();
+                bottomSheetDialog.dismiss();
+            }
+        });
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bottomSheetDialog.dismiss();
+            }
+        });
+        bottomSheetDialog.show();
     }
 
     private void setOnFinishButtonListener() {
