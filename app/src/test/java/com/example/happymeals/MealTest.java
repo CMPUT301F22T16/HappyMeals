0c62d577229cc5b5132134ed20adbb623fe39c68
Jayparkrighthere <zoeytwelve@gmail.com>
2022-11-24 23:16:42 -0700

    fix meal unit test

--- app/src/test/java/com/example/happymeals/MealTest.java
+++ app/src/test/java/com/example/happymeals/MealTest.java
@@ -1,75 +1,110 @@
-//package com.example.happymeals;
-//
-//import com.example.happymeals.meal.Meal;
-//
-//import org.junit.Assert;
-//import org.junit.Before;
-//import org.junit.Test;
-//
-//import java.util.ArrayList;
-//import java.util.HashMap;
-//import java.util.List;
-//
-//public class MealTest {
-//    Meal meal;
-//    Recipe recipe;
-//
-//    @Before
-//    public void mockMeal(){
-//        List<Recipe> recipes = new ArrayList<>();
-//        recipe = new Recipe();
-//        recipes.add(recipe);
-//        List<Double> scaling = new ArrayList<>();
-//        scaling.add(3.14);
-//        scaling.add(1.11);
-//        meal = new Meal(recipes,scaling,19.9);
-//    }
-//
-//    @Test
-//    public void testRemoveRecipe(){
-//        meal.removeRecipe(0);
-//        Assert.assertEquals(meal.getRecipes().size(),0);
-//    }
-//
-//    @Test
-//    public void testSetRecipe(){
-//        List<Recipe> r = new ArrayList<>();
-//        r.add(new Recipe());
-//        r.add(new Recipe());
-//        meal.setRecipes(r);
-//        Assert.assertEquals(meal.getRecipes().size(),2);
-//    }
-//
-//    @Test
-//    public void testaddRecipe(){
-//        Assert.assertEquals(1,meal.getRecipes().size());
-//        meal.addRecipe(recipe);
-//        Assert.assertEquals(2,meal.getRecipes().size());
-//
-//    }
-//
-//    @Test
-//    public void testGetRecipe(){
-//        List<Recipe> r = meal.getRecipes();
-//        Assert.assertEquals(r.get(0),recipe);
-//    }
-//
-//    @Test
-//    public void testGetScalings(){
-//        List<Double> scalings = meal.getScalings();
-//        Assert.assertTrue(scalings.get(0)==3.14);
-//        Assert.assertTrue(scalings.get(1)==1.11);
-//    }
-//
-//    @Test
-//    public void testGetCost(){
-//        Assert.assertTrue(meal.getCost()==19.9);
-//    }
-//
-//
-//    @Test
-//    public void testGetStorable(){
-//        HashMap<String, Object> data = meal.getStorable();
-//        Assert.assertEquals(data.get("cost"),19.9);
-//    }
-//}
+package com.example.happymeals;
+
+import com.example.happymeals.meal.Meal;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import kotlin.jvm.Throws;
+
+public class MealTest {
+    Meal meal;
+    Recipe recipe;
+
+    @Before
+    public void mockMeal(){
+        List<Recipe> recipes = new ArrayList<>();
+        recipe = new Recipe();
+        recipe.setR_id("test111id");
+        recipes.add(recipe);
+        Map<String,Double> scaling = new HashMap<>();
+        scaling.put(recipe.get_r_id(), 1.11);
+        meal = new Meal("Test Meal Title",recipes,scaling,19.9);
+    }
+
+    @Test
+    public void testRemoveRecipe(){
+        meal.removeRecipe(recipe);
+        Assert.assertEquals(meal.getRecipes().size(),0);
+    }
+
+    @Test
+    public void testSetRecipe(){
+        List<Recipe> r = new ArrayList<>();
+        r.add(new Recipe());
+        r.add(new Recipe());
+        meal.setRecipes(r);
+        Assert.assertEquals(meal.getRecipes().size(),2);
+    }
+
+    @Test
+    public void testaddRecipe(){
+        Assert.assertEquals(1,meal.getRecipes().size());
+        meal.addRecipe(recipe);
+        Assert.assertEquals(2,meal.getRecipes().size());
+
+    }
+
+    @Test
+    public void testGetRecipe(){
+        List<Recipe> r = meal.getRecipes();
+        Assert.assertEquals(r.get(0),recipe);
+    }
+
+    @Test
+    public void testSetScalings(){
+        Map<String,Double> scalings = new HashMap<>();
+        scalings.put("test222id",2.22);
+        scalings.put("test333id",3.33);
+        meal.setScalings(scalings);
+        Assert.assertTrue(scalings.get("test222id")==2.22);
+        Assert.assertTrue(scalings.get("test333id")==3.33);
+    }
+
+    @Test
+    public void testGetScalings(){
+        Map<String,Double> scalings = meal.getScalings();
+        Assert.assertTrue(scalings.get("test111id")==1.11);
+    }
+
+    @Test
+    public void testGetScalingForRecipe() throws Exception {
+        Double scaling = meal.getScalingForRecipe(recipe);
+        Assert.assertTrue(scaling == 1.11);
+    }
+
+    @Test
+    public void testGetScalingForRecipeException() throws Exception{
+        Recipe recipe2 = new Recipe();
+        recipe2.setR_id("test000id");
+        Exception e = Assert.assertThrows(Exception.class,()->meal.getScalingForRecipe(recipe2));
+        Assert.assertTrue(e.getMessage().contains("Recipe not found"));
+    }
+
+    @Test
+    public void testRemoveScalingForRecipe() {
+        meal.removeScalingForRecipe(recipe);
+        Assert.assertTrue(meal.getScalings().size()==0);
+    }
+
+
+
+
+    @Test
+    public void testGetCost(){
+        Assert.assertTrue(meal.getCost()==19.9);
+    }
+
+
+    @Test
+    public void testGetStorable(){
+        HashMap<String, Object> data = meal.getStorable();
+        Assert.assertEquals(data.get("cost"),19.9);
+    }
+}
