ec9602c96a45d812622fe4a707303cbd1832cec9
shehraj123 <shehrajsingh@gmail.com>
2022-11-27 14:13:32 -0700

    Add Storage test

---
+++ app/src/test/java/com/example/happymeals/StorageTest.java
@@ -0,0 +1,64 @@
+package com.example.happymeals;
+
+import com.example.happymeals.storage.Storage;
+
+import org.jetbrains.annotations.TestOnly;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+
+public class StorageTest {
+
+    Storage storage;
+
+    @Before
+    public void mockStorage() {
+        String name = "Fridge";
+        Storage storage = new Storage(name);
+        this.storage = storage;
+    }
+
+    @Test
+    public void testGetStoreName() {
+        mockStorage();
+        String name = this.storage.getStoreName();
+        Assert.assertEquals(name, "Fridge");
+    }
+
+    @Test
+    public void testSetStoreName() {
+        mockStorage();
+        String new_name = "Freezer";
+        this.storage.setStoreName(new_name);
+        Assert.assertEquals(this.storage.getStoreName(), new_name);
+    }
+
+    @Test
+    public void testGetItemCount() {
+        mockStorage();
+        Assert.assertEquals(this.storage.getItemCount(), 0);
+    }
+
+    @Test
+    public void testSetItemCount() {
+        mockStorage();
+        this.storage.setItemCount(1);
+        Assert.assertEquals(this.storage.getItemCount(), 1);
+    }
+
+    @Test
+    public void testGetStorable() {
+        mockStorage();
+        this.storage.setItemCount(1);
+        this.storage.setStoreName("Freezer");
+
+        HashMap<String, Object> data = this.storage.getStorable();
+        String name = (String) data.get("type");
+        int itemcount = (Integer) data.get("items");
+
+        Assert.assertEquals(name, "Freezer");
+        Assert.assertEquals(itemcount, 1);
+    }
+}
