dfbf4b5908b95cb96b9838fbdb025eef83f6568d
Jayparkrighthere <zoeytwelve@gmail.com>
2022-11-27 18:02:18 -0700

    test init

---
+++ app/src/androidTest/java/com/example/happymeals/MealIntentTest.java
@@ -0,0 +1,48 @@
+package com.example.happymeals;
+
+import android.app.Activity;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+
+import com.example.happymeals.meal.MPMyMealsActivity;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.robotium.solo.Solo;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class MealIntentTest {
+    private Solo solo;
+    private FirebaseFirestore conn;
+
+    @Rule
+    public ActivityTestRule<MainActivity> rule = new ActivityTestRule<>(MainActivity.class, true, true);
+
+    /**
+     * Runs before all tests and creates solo instance.
+     * @throws Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+        solo = new Solo(InstrumentationRegistry.getInstrumentation(), rule.getActivity());
+        solo.clickOnView(solo.getView(R.id.meal_button));
+        solo.sleep(3000);
+        solo.assertCurrentActivity("Wrong Activity", MPMyMealsActivity.class);
+    }
+
+    /**
+     * Gets the Activity
+     * @throws Exception
+     */
+    @Test
+    public void start() throws Exception {
+        Activity activity = rule.getActivity();
+    }
+
+
+}
